f476e5af787c8cd2eb8a50bbc917c582f9bff327
Guilherme Toshio Miyake <toshio@dinerama.com.br>
2023-05-06 14:15:40 -0300

    lint

--- src/deps.ts
+++ src/deps.ts
@@ -2,7 +2,10 @@ export * as jose from "https://deno.land/x/jose@v4.14.4/index.ts";
 export type {
   CookieMapOptions,
   CookieMapSetDeleteOptions,
-  Headered
+  Headered,
 } from "https://deno.land/std@0.185.0/http/cookie_map.ts";
 
-export { CookieMap, mergeHeaders } from "https://deno.land/std@0.185.0/http/cookie_map.ts";
+export {
+  CookieMap,
+  mergeHeaders,
+} from "https://deno.land/std@0.185.0/http/cookie_map.ts";
